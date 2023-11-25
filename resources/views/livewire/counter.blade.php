092ca8c7258e7074b233f298722a351de3fd0dc2
andrii <andrii.romanyshyn@jetsoftpro.com>
2023-11-25 23:43:17 +0100

    feat(filament):
    Install Filament

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,8 @@
 <div>
     {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <h1>{{ $count }}</h1>
+
+    <button wire:click="increment">+</button>
+
+    <button wire:click="decrement">-</button>
 </div>
