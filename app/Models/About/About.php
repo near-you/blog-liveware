99ebc416ddccd0be08eb95e0ab40760b1d2cfbde
NearYou <a23.romanyshyn@gmail.com>
2024-06-19 19:57:50 +0200

    changes before refactoring

--- app/Models/About/About.php
+++ app/Models/About/About.php
@@ -5,10 +5,7 @@ namespace App\Models\About;
 use App\Actions\GetImages;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasManyThrough;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 
 class About extends Model
--- database/migrations/2024_06_05_210428_create_active_menu_items_table.php
+++ database/migrations/2024_06_05_210428_create_active_menu_items_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('active_menu_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
